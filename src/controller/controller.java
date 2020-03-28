e5a3d1190d74f36fa2a69dc202f0fd79b6e09e53
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2020-03-28 14:38:06 +0900

    Update controller.java

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -2,7 +2,7 @@ package controller;
 
 public class controller implements Resource {
 
-    public void process(){
+    public void process() {
         System.out.println("controller 실행");
         main.show();
     }
